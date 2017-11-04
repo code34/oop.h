f671bf82bdf39b28f4d7825027cf44cfd3c3a814
code34 <nicolas_boiteux@yahoo.fr>
2017-11-04 23:15:10 +0100

    update the licence information

--- oop.h
+++ oop.h
@@ -1,41 +1,23 @@
 /*
-	This file is part of RBS (Realistic Battleground Simulator).
 
-	Copyright © 2013 RBS Development Team.
-	All rights reserved.
+Author: code34 <nicolas_boiteux@yahoo.fr>
+Author: Naught <dylanplecki@gmail.com>
 
-	RBS is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+Copyright (C) 2013-2018 Nicolas BOITEUX
 
-	RBS is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+	
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 */
-
-//////////////////////////////////////////////////////////////
-//
-//	Title: RBS Object Oriented SQF Scripting
-//	----------------------------------------
-//	File: oop.h
-//	Author: Naught <dylanplecki@gmail.com>
-//	Version: 1.3.1
-//
-//	Description:
-//	Contains preprocessor definitions and macros for designing
-//	and implementing object oriented code into the SQF
-//	scripting language. Uses global variables.
-//
-//	Note:
-//	All API documentation can be found below in the
-//	<Interactive (API) Macros and Definitions> group.
-//
-//////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////
 //  Group: Basic Macros
